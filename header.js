4250fed0e89cd0474b58ba91f94b7c86b87bb5f2
akyua <gabrielnovatoo@gmail.com>
2022-11-16 17:30:16 -0300

    Alteração das imagens do footer e arrumando lógica do header js

--- header.js
+++ header.js
@@ -1,11 +1,23 @@
-var lastScrollTop = 0;
+var lastScrollTop = 0, scrollPx = 0, accumulativeScroll = 0;
 menu = document.getElementById("menu");
 window.addEventListener("scroll", function(){
-    var scrollTop = window.pageYOffset || document .documentElement.scrollTop;
-    if (scrollTop > lastScrollTop){
-        menu.style.top="-80px";
+    var scrollTop = window.pageYOffset;
+    if (scrollTop > lastScrollTop) {
+        // desceu
+        scrollPx = window.pageYOffset - lastScrollTop;
+        accumulativeScroll += scrollPx;
+        if (accumulativeScroll >= 80) {
+            accumulativeScroll = 80;
+        }
+        menu.style.top="-" + accumulativeScroll + "px";
     } else {
-        menu.style.top="0px";
+        // subiu
+        scrollPx = lastScrollTop - window.pageYOffset;
+        accumulativeScroll -= scrollPx;
+        if (-accumulativeScroll >= 0) {
+            accumulativeScroll = 0;
+        }
+        menu.style.top="-" + accumulativeScroll + "px";
     }
     lastScrollTop = scrollTop;
 })
