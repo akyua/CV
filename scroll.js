e2a142de173dd058ac46d0ead4b35bc89603037b
akyua <gabrielnovatoo@gmail.com>
2022-11-17 19:01:50 -0300

    Scrol porém ainda com bug

---
+++ scroll.js
@@ -0,0 +1,9 @@
+sr = ScrollReveal({ reset:true});
+ScrollReveal({ distance: '15px' });
+sr.reveal('.title_description', {duration : 3000, origin: 'right'})
+sr.reveal('.aboutme_title', {duration : 3000, origin: 'left'})
+sr.reveal('.aboutme_description', {duration : 3000, origin: 'left'})
+sr.reveal('.experience_title', {duration : 3000, origin: 'right'})
+sr.reveal('.experience_list', {duration : 3000, origin: 'right'})
+sr.reveal('#projects', {duration : 3000, origin:'bottom'})
+sr.reveal('#skills', {duration : 3000, origin:'left'})
