ba27c11dc22c0f36dd57736b00491cc0ea713ff9
akyua <gabrielnovatoo@gmail.com>
2022-11-17 20:08:37 -0300

    Terminando animações

--- scroll.js
+++ scroll.js
@@ -4,10 +4,16 @@ ScrollReveal({
         top: 0
     }
 });
-sr.reveal('.title_description', {duration : 3000, origin: 'right'})
-sr.reveal('.aboutme_title', {duration : 3000, origin: 'left'})
-sr.reveal('.aboutme_description', {duration : 3000, origin: 'left'})
-sr.reveal('.experience_title', {duration : 3000, origin: 'right'})
-sr.reveal('.experience_list', {duration : 3000, origin: 'right'})
-sr.reveal('#projects', {duration : 3000, origin:'bottom'})
-sr.reveal('#skills', {duration : 3000, origin:'left'})
+sr.reveal('.title_name', {duration : 2000, origin: 'right'})
+sr.reveal('.title_description', {duration : 2000, origin: 'right'})
+sr.reveal('.aboutme_title', {duration : 2000, origin: 'left'})
+sr.reveal('.aboutme_description', {duration : 2000, origin: 'left'})
+sr.reveal('.experience_title', {duration : 2000, origin: 'right'})
+sr.reveal('.experience_list', {duration : 2000, origin: 'right'})
+sr.reveal('.projects_title', {duration : 2000, origin:'bottom'})
+sr.reveal('.projects_description', {duration : 2000, origin:'left'})
+sr.reveal('.projects_subtitle', {duration : 2000, origin:'left'})
+sr.reveal('.skills_title', {duration : 2000, origin:'left'})
+sr.reveal('.skills_subtitle', {duration : 2000, origin:'left'})
+sr.reveal('.skills_text', {duration : 2000, origin:'left'})
+sr.reveal('footer', {duration : 2000, origin:'bottom'})
